eb2ff616a92510fcbcb89c6b8b3a96f4d207080a
Lesuuda Ljamani <lesuudaljamani@gmail.com>
2023-03-19 11:13:34 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print numbers in ascending order
+ * Return: 0(success)
+ */
+int main(void)
+{
+
+int a;
+
+for (a = 10; a <= 10; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
