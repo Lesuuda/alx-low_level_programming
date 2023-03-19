31cdbe400157d68295529770c21a0d876b0faf52
Lesuuda Ljamani <lesuudaljamani@gmail.com>
2023-03-19 12:06:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return:0(success)
+ */
+int main(void)
+{
+
+int a;
+
+a = 100;
+while (a < 10)
+{
+putchar(a + '0');
+a++;
+}
+putchar('\n');
+return (0);
+}
