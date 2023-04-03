3d3370616ea90f6f1c7a330ab8c45b65e923436d
Lesuuda <lesuudaljamani@gmail.com>
2023-04-03 14:58:17 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,7 +14,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i <= (size * size); i = i + size + 1)
 		sum1 = sum1 + a[i];
-	for (i = 0; i <= (size * size) - size; n = n + size - 1)
+	for (n = 0; n <= (size * size) - size; n = n + size - 1)
 		sum2 = sum2 + a[n];
 	printf("%d, %d\n", sum1, sum2);
 }
