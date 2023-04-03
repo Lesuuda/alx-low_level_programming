59d70b1d108e39b4af259ea7a8557f73ba94a6d3
Lesuuda Ljamani <lesuudaljamani@gmail.com>
2023-04-03 02:26:07 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - this is the entry point
+ * @n: Number of byte of memory
+ * @s: pointer to te memory
+ * @b: the  costant bytes
+ * Return: s, a pointer to the memory
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int n;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
