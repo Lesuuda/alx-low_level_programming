912983b08a48c03bb0be62f70a1413ad13cdc2a2
Lesuuda <lesuudaljamani@gmail.com>
2023-04-03 04:55:40 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies data from one location to another
+ * @n: number of bytes
+ * @src: the source location
+ * @dest: the destination location
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
