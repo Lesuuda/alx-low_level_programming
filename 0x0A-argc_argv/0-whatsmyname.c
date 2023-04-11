0feb93cf52bedd4718d6266a35fc34b701cabab9
Lesuuda <lesuudaljamani@gmail.com>
2023-04-11 00:05:25 -0700

    number one

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: integer
+ * @argv: character
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
