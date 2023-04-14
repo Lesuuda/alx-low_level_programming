dbe5321a298a64c1e1cd0df66fbaac6d0f3e19c5
Lesuuda <lesuudaljamani@gmail.com>
2023-04-14 03:50:05 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: integer
+ * @argv: character array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+	int args;
+
+	sum = 0;
+	i = 0;
+	args = atoi(argv[i]);
+
+	for (i = 0; i < argc; i++)
+	{
+		sum = sum + args;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
