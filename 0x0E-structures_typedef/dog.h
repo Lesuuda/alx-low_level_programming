4fc623cc73e08d565d31827b43f1f76f4909a1d4
Lesuuda <lesuudaljamani@gmail.com>
2023-04-17 10:14:57 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - main entry point
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
