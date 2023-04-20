d1b2245cd653987ce0afa1630465c126deeee9e2
Lesuuda <lesuudaljamani@gmail.com>
2023-04-20 13:29:05 -0700

    alx-low_level_programming/0x10-variadic_functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - function to find the sum of input parameters
+ * @n: the number of parameters
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+        unsigned int sum;
+
+        sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+
+	va_end(ap);
+	return (sum);
+}
+
