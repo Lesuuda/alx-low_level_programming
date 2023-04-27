da3b2581b65af4041d88c8df6b0a1b9c2328e3c1
Lesuuda <lesuudaljamani@gmail.com>
2023-04-27 11:58:47 -0700

    ADVANCED

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * first - prints a sentence before main is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
