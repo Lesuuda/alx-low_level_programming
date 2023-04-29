36a8b1e3deaaa9ddea8d641dd66bce1e9acaabdc
Lesuuda <lesuudaljamani@gmail.com>
2023-04-29 07:56:18 -0700

    printint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	const listint_t *temp = h;
 
 
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp  = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 
 	}
