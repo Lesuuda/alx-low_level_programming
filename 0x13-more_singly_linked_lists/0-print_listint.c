a4b91dd9978e601b6d6bdd4810eca8cb290b1b4b
Lesuuda <lesuudaljamani@gmail.com>
2023-04-29 06:09:59 -0700

    printlist

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of listint_t
+ * @h: pointer to new struct
+ * Return: number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	const listint_t *temp = h;
+
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp  = temp->next;
+		count++;
+
+	}
+	return (count);
+
+}
