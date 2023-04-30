cd5349a46f93ee73e541f5add6f19d69ea8f62dd
Lesuuda <lesuudaljamani@gmail.com>
2023-04-30 02:38:34 -0700

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,10 +8,11 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 	const listint_t *temp = h;
+
 	while (temp != NULL)
 	{
-		count++;
 		temp = temp->next;
+		count++;
 	}
 	return (count);
 }
