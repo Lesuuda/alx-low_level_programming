c1d2a0f7fb7b91a65cb2df74b5ad12996620f270
lesuuda <lesuudaljamani@gmail.com>
2023-10-02 11:46:43 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,30 +7,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t fdread, fdwrite, fdclose;
+	ssize_t fd;
+	ssize_t fdread, fdwrite;
 	char *space;
-
-	if (filename == NULL)
-		return (0);
-	space = malloc(sizeof(char) * letters);
-	if (space == NULL)
-	{
-		return (-1);
-	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	space = malloc(sizeof(char) * letters);
+
 	fdread = read(fd, space, letters);
-	if (fdread == -1)
-		return (-1);
+
 	fdwrite = write(STDOUT_FILENO, space, fdread);
 
-	if (fdwrite == -1)
-		return (-1);
-	fdclose = close(fd);
-	if (fdclose == -1)
-		return (-1);
-	return (fdread);
+	free(space);
+	close(fd);
+
+	return (fdwrite);
 }
