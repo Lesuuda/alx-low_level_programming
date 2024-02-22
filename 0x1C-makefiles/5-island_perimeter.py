da01de6e35c14383956a39b82623c7c5d2df3da1
lesuuda <lesuudaljamani@gmail.com>
2024-02-22 21:05:05 +0300

    fixed pycodestyle for island perimter question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,12 +9,13 @@ def island_perimeter(grid):
     function that finds the perimeter of an island
     """
     visited = set()
+
     def dfs(i, j):
         """
         depth first search algorithm that iterates through
         our island grid"""
         if i >= len(grid) or j >= len(grid[0]) or \
-        i < 0 or j < 0 or grid[i][j] == 0:
+                i < 0 or j < 0 or grid[i][j] == 0:
             return 1
         if (i, j) in visited:
             return 0
@@ -28,4 +29,3 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j]:
                 return dfs(i, j)
-
