8b8ade94b4c243d745289f2a13c2a42476c33b00
lesuuda <lesuudaljamani@gmail.com>
2024-04-09 12:30:16 +0300

    fix: added print method for value being checked

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,8 +13,13 @@ int linear_search(int *array, size_t size, int value)
 
 	i = 0;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d]\n", array[i]);
 		if (array[i] == value)
 		{
 			return (array[i]);
