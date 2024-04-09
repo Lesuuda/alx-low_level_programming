c189906938acb6d2e725d1704362729bd3795571
lesuuda <lesuudaljamani@gmail.com>
2024-04-09 12:22:21 +0300

    core: added readme and function to perform linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * linear_search - performs linear search algorithm on the array
+ * @array: pointer to the first element of the array to be searched
+ * @size: size of the array
+ * @value: the value to searched for
+ * Return: index of the array if found and -1 if not
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (array[i]);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+int linear_search(int *array, size_t size, int value);
+#endif
